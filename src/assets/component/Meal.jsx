80bbf7aae6b16be626351e96342172cb6a62ef8d
KBandipo~ <keneebandipo@gmail.com>
2023-11-06 16:35:36 +0100

    Add props to the Meal contents (#11)

--- src/assets/component/Meal.jsx
+++ src/assets/component/Meal.jsx
@@ -1,17 +1,17 @@
-function Meal() {
+function Meal(props) {
   return (
     <>
       <div className="m-[30px] flex w-[500px] justify-between overflow-hidden rounded bg-[#f7f7f7] p-4 shadow-lg hover:shadow-2xl">
         <img
-          src="/images/amala.jpg"
-          alt="amala with ewedu and gbediri image"
+          src={props.photoname}
+          alt={props.photoalt}
           className="h-[300px] w-[60%] rounded-md object-cover "
         />
         <div className="w-[35%]">
-          <h2 className="text-lg font-bold"> Amala</h2>
-          <p>Amala ewedu and gbegiri</p>
+          <h2 className="text-lg font-bold"> {props.mealName}</h2>
+          <p>{props.mealCombination}</p>
           <p className="font-semibold">
-            <span>Price: </span> N5000
+            <span>Price: </span> N{props.price}
           </p>
         </div>
       </div>
