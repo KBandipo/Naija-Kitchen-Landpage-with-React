4cad4dc9ef6ce1bcca7066c371e9e8833f468e9e
KBandipo~ <keneebandipo@gmail.com>
2023-11-06 23:00:48 +0100

    Edit the Footer (#10)

--- src/assets/component/Footer.jsx
+++ src/assets/component/Footer.jsx
@@ -1,12 +1,13 @@
 function Footer() {
-  const hour = new Date().getHours();
+  let hour = new Date().getHours();
+  let presentTime = new Date().toLocaleTimeString();
   const openHour = 6;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <footer className="text-[ #e6eff9] mx-auto bg-[#004200] ">
       <h1 className="text-center">
-        {new Date().toLocaleTimeString()}, {new Date().toDateString()},{" "}
+        {presentTime}, {new Date().toDateString()},{" "}
         {isOpen ? (
           <p>We are open until {closeHour + 1}:00. Come visit us to order</p>
         ) : (
