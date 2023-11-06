ffbfb8c063bb964e7f6aaa02f12b1682bb4241e6
KBandipo~ <keneebandipo@gmail.com>
2023-11-06 14:03:15 +0100

    Create Meal file)

---
+++ src/assets/component/Meal.jsx
@@ -0,0 +1,13 @@
+function Meal() {
+  return (
+    <>
+      <img src="/images/amala.jpg" alt="amala with ewedu and gbediri image" />
+      <div>
+        <h2> Amala with ewedu and gbediri</h2>
+        <p>Amala ewedu and gbegiri</p>
+      </div>
+    </>
+  );
+}
+
+export default Meal;
