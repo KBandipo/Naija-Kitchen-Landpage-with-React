090e7538e53e462b3eb8a70744c19dfb0a131135
KBandipo~ <keneebandipo@gmail.com>
2023-11-06 21:10:38 +0100

    Style Footer section (#10)

--- src/assets/component/Footer.jsx
+++ src/assets/component/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-  return <footer>We're currently open!</footer>;
+  return (
+    <footer className="text-[ #e6eff9] mx-auto bg-[#004200] ">
+      <h1 className="text-center">We are open!</h1>
+    </footer>
+  );
 }
 
 export default Footer;
