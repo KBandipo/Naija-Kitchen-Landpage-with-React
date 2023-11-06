0846cbaf213c5a156647bc40d715864d32dbebd1
KBandipo~ <keneebandipo@gmail.com>
2023-11-06 23:00:21 +0100

    Add time and date to Footer using tenary condition (#10)

--- src/assets/component/Footer.jsx
+++ src/assets/component/Footer.jsx
@@ -1,9 +1,21 @@
-import React from "react";
-
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 6;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <footer className="text-[ #e6eff9] mx-auto bg-[#004200] ">
-      <h1 className="text-center">We are open!</h1>
+      <h1 className="text-center">
+        {new Date().toLocaleTimeString()}, {new Date().toDateString()},{" "}
+        {isOpen ? (
+          <p>We are open until {closeHour + 1}:00. Come visit us to order</p>
+        ) : (
+          <p>
+            We are closed for today, we are happy to welcome you between{" "}
+            {openHour}:00 and {closeHour + 1}:00
+          </p>
+        )}
+      </h1>
     </footer>
   );
 }
