03452b9299e6ce898818c3b75fccab8155163a16
KBandipo~ <keneebandipo@gmail.com>
2023-11-06 17:47:10 +0100

    Display the images in grid (#11)

--- src/assets/component/Menu.jsx
+++ src/assets/component/Menu.jsx
@@ -4,41 +4,43 @@ import Meal from "./Meal";
 function Menu() {
   return (
     <>
-      <div className="text-stone-700">
+      <div className="my-[100px] text-stone-700">
         <div className="flex justify-center ">
           <h1 className="border-b-2 border-t-2 border-solid border-stone-500  text-center text-xl">
             OUR MENU
           </h1>
         </div>
-        <Meal
-          photoname="/images/amala.jpg"
-          photoalt="amala with ewedu and gbediri image"
-          mealName="Amala"
-          mealCombination="Amala ewedu and gbegiri"
-          price="7000"
-        />
-        <Meal
-          photoname="/images/pounded-yam-with-egusi-soup.jpg"
-          photoalt="pounded-yam-with-egusi-soup image"
-          mealName="Pounded yam"
-          mealCombination="Pounded yam with egusi soup"
-          price="8000"
-        />
+        <div className="grid grid-cols-2">
+          <Meal
+            photoname="/images/amala.jpg"
+            photoalt="amala with ewedu and gbediri image"
+            mealName="Amala"
+            mealCombination="Amala ewedu and gbegiri"
+            price="7000"
+          />
+          <Meal
+            photoname="/images/pounded-yam-with-egusi-soup.jpg"
+            photoalt="pounded-yam-with-egusi-soup image"
+            mealName="Pounded yam"
+            mealCombination="Pounded yam with egusi soup"
+            price="8000"
+          />
 
-        <Meal
-          photoname="/images/Ekuru.jpg"
-          photoalt="Ekuru image"
-          mealName="Ekuru"
-          mealCombination="Ekuru with stew"
-          price="7000"
-        />
-        <Meal
-          photoname="/images/eba.jpg"
-          photoalt="Eba image"
-          mealName="Eba"
-          mealCombination="Eba with Efo-riru"
-          price="8000"
-        />
+          <Meal
+            photoname="/images/Ekuru.jpg"
+            photoalt="Ekuru image"
+            mealName="Ekuru"
+            mealCombination="Ekuru with stew"
+            price="7000"
+          />
+          <Meal
+            photoname="/images/eba.jpg"
+            photoalt="Eba image"
+            mealName="Eba"
+            mealCombination="Eba with Efo-riru"
+            price="8000"
+          />
+        </div>
       </div>
     </>
   );
