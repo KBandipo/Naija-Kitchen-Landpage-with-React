2cccb77d4a23540d1bff745fb62306e7ca13cfa2
KBandipo~ <keneebandipo@gmail.com>
2023-11-06 10:54:52 +0100

    Create the content for HeroSection (#6)

---
+++ src/assets/component/WelcomeMessage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function WelcomeMessage() {
+  return (
+    <>
+      <div className="container m-20 my-auto w-[50%] rounded-md bg-[#004200] p-[30px] text-center text-[#e6eff9]">
+        <div className="container ">
+          <h1 className=" mx-auto mb-4 text-4xl font-extrabold">
+            Discover the Soul of Nigeria on Your Plate
+          </h1>
+          <p className="text-md mx-auto">
+            Welcome to Naija Kichen, where the vibrant flavors and rich
+            traditions of Nigeria come together to create an extraordinary
+            dining experience. Step into a world of bold spices, aromatic
+            dishes, and warm hospitality. We invite you to embark on a culinary
+            journey that transcends borders, savoring the true essence of
+            Nigeria's diverse cuisine.Ekabo o! (You are very welcome!)
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default WelcomeMessage;
