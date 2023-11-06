69604e58e648409cbbc3ff5da387489f6ca4acc4
KBandipo~ <keneebandipo@gmail.com>
2023-11-07 00:35:13 +0100

    Arrange the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import NavBar from "./assets/component/NavBar";
+import MainBody from "./assets/component/MainBody";
+import Footer from "./assets/component/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+      <MainBody />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
