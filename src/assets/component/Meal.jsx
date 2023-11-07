c1057dd91e73671904183d373cbe8b612b7af481
KBandipo~ <keneebandipo@gmail.com>
2023-11-07 11:33:47 +0100

    Edit the  responsiveness of the Meal contents(15)

--- src/assets/component/Meal.jsx
+++ src/assets/component/Meal.jsx
@@ -4,17 +4,17 @@ function Meal(props) {
   const { photoname, photoalt, mealName, mealCombination, price } = props;
   return (
     <>
-      <div className="m-[30px] flex w-[500px] justify-between overflow-hidden rounded bg-[#f7f7f7] p-4 shadow-lg hover:shadow-2xl">
+      <div className="m-[20px] w-[400px] justify-between overflow-hidden rounded bg-[#f7f7f7] p-4 shadow-lg hover:shadow-2xl lg:flex lg:w-[450px] ">
         <img
           src={photoname}
           alt={photoalt}
-          className="h-[300px] w-[60%] rounded-md object-cover "
+          className="h-[200px] w-[60%] rounded-md object-cover lg:h-[300px] lg:w-[60%]"
         />
-        <div className="w-[35%]">
-          <h2 className="mb-[5px] text-lg font-bold"> {mealName}</h2>
-          <p className="font-semibold">Contents</p>
-          <h3 className="mb-[5px]">{mealCombination}</h3>
-          <p className="font-semibold">
+        <div className="mx-4 mx-auto flex-1 md:w-2/3">
+          <h2 className="mb-2 text-xl font-bold"> {mealName}</h2>
+          <p className="mb-2 font-semibold">Contents</p>
+          <h3 className="mb-2">{mealCombination}</h3>
+          <p className="mb-2 font-semibold">
             <span>Price: </span> N{price}
           </p>
           <Button />
