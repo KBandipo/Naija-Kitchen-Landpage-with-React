6aba0d55b5d4aeaf94e12c962b2d8f3499029ff8
KBandipo~ <keneebandipo@gmail.com>
2023-11-07 13:38:05 +0100

    Add more text to WelcomeMessage

--- src/assets/component/WelcomeMessage.jsx
+++ src/assets/component/WelcomeMessage.jsx
@@ -4,7 +4,8 @@ function WelcomeMessage() {
       <div className="container mx-auto my-auto w-full rounded-md bg-[#0042009f] p-6 text-center text-[#e6eff9] md:w-1/2 md:p-10">
         <div className="container mx-auto ">
           <h1 className=" mx-auto mb-4 text-2xl font-extrabold md:text-4xl">
-            Discover the Soul of Nigeria on Your Plate
+            Discover the Soul of Nigeria on Your Plate. Barka da zuwa! Nnọọ! Ẹ
+            ku abọ!
           </h1>
           <p className="text-md mx-auto md:text-lg">
             Welcome to Naija Kichen, where the vibrant flavors and rich
@@ -12,7 +13,8 @@ function WelcomeMessage() {
             dining experience. Step into a world of bold spices, aromatic
             dishes, and warm hospitality. We invite you to embark on a culinary
             journey that transcends borders, savoring the true essence of
-            Nigeria's diverse cuisine.Ekabo o! (You are very welcome!)
+            Nigeria's diverse cuisine. Barka da zuwa! Nnọọ! Ẹ ku abọ! (You are
+            very welcome!)
           </p>
         </div>
       </div>
