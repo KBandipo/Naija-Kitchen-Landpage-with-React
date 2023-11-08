bcd1f067ee64c1cbdcfbfc7ad97f9704c16d2d07
KBandipo~ <keneebandipo@gmail.com>
2023-11-08 09:36:21 +0100

    Refactor code

--- src/assets/component/Footer.jsx
+++ src/assets/component/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
   let hour = new Date().getHours();
   let presentTime = new Date().toLocaleTimeString();
   const openHour = 8;
-  const closeHour = 22;
+  const closeHour = 21;
   const isOpen = hour >= openHour && hour <= closeHour;
   return (
     <footer className="text-[ #e6eff9] mx-auto bg-[#004200] ">
@@ -12,7 +12,7 @@ function Footer() {
           <p>We are open until {closeHour + 1}:00HR. Come visit us to order</p>
         ) : (
           <p>
-            We are closed for today, we are happy to welcome you between{" "}
+            We are closed for the day, we are happy to welcome you between{" "}
             {openHour}:00HR and {closeHour + 1}:00HR Monday-Friday
           </p>
         )}
