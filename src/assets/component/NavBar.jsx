86f6eb37f4265de3ec6ac199d7e3c6fad7289414
KBandipo~ <keneebandipo@gmail.com>
2023-11-17 08:45:16 +0100

    Change icon

--- src/assets/component/NavBar.jsx
+++ src/assets/component/NavBar.jsx
@@ -103,14 +103,14 @@ function NavBar() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke=" #e6eff9"
-                className="h-6 w-6"
+                stroke-width="1.5"
+                stroke="#e6eff9"
+                class="h-6 w-6"
               >
                 <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16m-7 6h7"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
                 />
               </svg>
             </button>
